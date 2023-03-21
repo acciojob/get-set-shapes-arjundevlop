e150456f0f5f58bdd1ce0b4291b93d192253c6b9
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-21 16:29:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 //complete this code
 class Rectangle {
-	constructor(width,height){
-		this._width = width;
-		this._height = height;
-	}
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 
-	get width(){
-		return this._width;
-	}
-	get height(){
-		return this._height;
-	}
-	getArea(){
-		return (2*this.width * this.height);
-	}
+  getWidth() {
+    return this.width;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
 }
 
-class Square extends Animal {
-	getPerimeter(){
-		return (4*this._height);
-	}
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+    this.side = side;
+  }
+
+  getSide() {
+    return this.side;
+  }
+
+  getPerimeter() {
+    return 4 * this.side;
+  }
 }
 
 // Do not change the code below this line
