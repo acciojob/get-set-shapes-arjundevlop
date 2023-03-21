ede2497a8139161e65a0fce017a6fec719d10fe6
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-21 16:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	get width(){
+		return this._width;
+	}
+	get height(){
+		return this._height;
+	}
+	getArea(){
+		return (2*this.width * this.height);
+	}
+}
+
+class Square extends Animal {
+	getPerimeter(){
+		return (4*this._height);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
